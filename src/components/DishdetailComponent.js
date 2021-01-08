a04d7b667bd5aa44d929dff58546f14e4ed5e4b2
Manh <ptmanh99@gmail.com>
2021-01-08 23:40:29 +0700

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,69 +1,80 @@
 import React from 'react'
 import {
-    Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 
-    function RenderDish(dish) {
-        if (dish != null)
-            return (
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return (
-                <div></div>
-            );
-    }
+function RenderDish({dish}) {
+    if (dish != null)
+        return (
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    else
+        return (
+            <div></div>
+        );
+}
 
-   function RenderComments(comments){
-        if (comments != null)
-            return(
-                <div >
-                    <ul className = "list-unstyled">
-                        {comments.map(comment =>
-                            <div >
-                                <li>
-                                    {comment.comment}
-                                </li>
-                                <li>
-                                    -- {comment.author} ,
+function RenderComments({comments})  {
+    if (comments != null)
+        return (
+            <div >
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {comments.map(comment =>
+                        <div >
+                            <li>
+                                {comment.comment}
+                            </li>
+                            <li>
+                                -- {comment.author} ,
                                     {moment(comment.date).add(1, 'd').format(' MMM DD, YYYY')}
-                                </li>
-                                <br></br>
-                            </div>
-                        )}
-                   </ul>
-                </div>
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
+                            </li>
+                            <br></br>
+                        </div>
+                    )}
+                </ul>
+            </div>
+        );
+    else
+        return (
+            <div></div>
+        );
+}
 
-   const DishDetail = (props) => {
-        if(props.dish != null)
+    const DishDetail = (props) => {
+        if (props.dish != null || props.comments != null)
             return (
-                <div class="container">
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
-                            {RenderDish(props.dish)}
+                            <RenderDish dish={props.dish} />
                         </div>
                         <div className="col-12 col-md-5 m-1">
-                            <h4>Comments</h4>
-                            {RenderComments(props.dish.comments)}
+                            <RenderComments comments={props.comments} />
                         </div>
                     </div>
                 </div>
-            )
-        else 
+            );
+        else
             return (
                 <div class="container"></div>
             )
--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -4,6 +4,7 @@ import Footer  from "./FooterComponent";
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent'
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -28,6 +29,13 @@ class MainComponents extends Component {
 
   render() {
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     const HomePage = () => {
       return(
         <Home 
@@ -37,13 +45,14 @@ class MainComponents extends Component {
         />
       );
     }
-    
+
     return (
         <div>
             <Header />
             <Switch>
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />
             <Redirect to="/home" />
             </Switch>
